b4e6ca5c61c3b7296d1604504393c3b01e465e69
hendyardhana <hendy.ardha1708@gmail.com>
2023-05-17 13:12:53 +0700

    Initial commit

---
+++ app/src/main/java/ac/id/ubaya/informatika/s160419137/todoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ac.id.ubaya.informatika.s160419137.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
